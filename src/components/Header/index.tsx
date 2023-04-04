974b495f460dbe2efc619c10272b75ba799b9d6a
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-04 19:52:28 -0300

    feat: este commit adicionará o tsx e styles do componente header.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import * as S from "./styles";
+
+import Button from "../Button";
+
+import logoSvg from "../../assets/images/svg/logo.svg";
+import { Plus } from "phosphor-react";
+
+const Header = () => {
+  return (
+    <S.HeaderContainer>
+      <S.HeaderContent>
+        <img
+          src={logoSvg}
+          alt="Logotipo DT Money"
+          title="Logotipo DT Money"
+          aria-label="Logotipo DT Money"
+        />
+
+        <Button type="button" icon={<Plus weight="bold" />}>
+          Nova Transação
+        </Button>
+      </S.HeaderContent>
+    </S.HeaderContainer>
+  );
+};
+
+export default Header;

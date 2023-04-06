a427fa215aff5afa41aef7677848591bf960a84d
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-06 09:30:30 -0300

    feat: este commit irá adicionar o export default para o componente summaryCard.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Header } from "./Header";
 export { default as Summary } from "./Summary";
+export { default as SummaryCard } from "./SummaryCard";

be6a73fa9c14932e9d5203ec90db1ad8e15a2be6
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-06 09:32:39 -0300

    feat: este commit irá adicionar o tsx, styles e types do componente summaryCard

---
+++ src/components/SummaryCard/index.tsx
@@ -0,0 +1,24 @@
+import * as S from "./styles";
+
+import { SummaryCardProps } from "./summaryCard";
+
+const SummaryCard = ({
+  title,
+  icon,
+  value = "R$ 0,00",
+  variant = "default"
+}: SummaryCardProps) => {
+  return (
+    <S.Card variant={variant} role="card" aria-label="Summary Card">
+      <S.Header>
+        <h4>{title}</h4>
+
+        {!!icon && icon}
+      </S.Header>
+
+      <S.Text>{value}</S.Text>
+    </S.Card>
+  );
+};
+
+export default SummaryCard;

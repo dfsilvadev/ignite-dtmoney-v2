4f401c2e0b23c5fd9f50c93067e7c6169009776c
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-07 15:52:44 -0300

    feat: este commit irá adicionar o tsx e styles do template base.

---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Header } from "../../components";
+
+import { BaseTemplateProps } from "./base";
+
+const Base = ({ children }: BaseTemplateProps) => {
+  return (
+    <div>
+      <Header />
+      <Container>{children}</Container>
+    </div>
+  );
+};
+
+export default Base;

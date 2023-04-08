50e77633b4bdd687724c72ce463a4c2e81df68af
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-08 15:23:51 -0300

    feat: este commit irá adicionar o tsx, styles e types do componente search form.

---
+++ src/components/SearchForm/index.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { MagnifyingGlass } from "phosphor-react";
+
+import Button from "../Button";
+
+import * as S from "./styles";
+
+import { SearchFormProps } from "./searchForm";
+
+const SearchForm = ({ onSubmit, ...props }: SearchFormProps) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const inputtedValue = evt.currentTarget.value;
+
+    setValue(inputtedValue);
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    !!onSubmit && onSubmit();
+  };
+
+  return (
+    <S.SearchFormContainer {...props} onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Busque uma transação"
+        value={value}
+        aria-label="search"
+        onChange={handleChange}
+      />
+      <Button type="submit" ghost icon={<MagnifyingGlass weight="bold" />}>
+        Buscar
+      </Button>
+    </S.SearchFormContainer>
+  );
+};
+
+export default SearchForm;

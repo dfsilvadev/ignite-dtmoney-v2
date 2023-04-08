e57807ae3b0abc1988f7eaa140a87308a2d8a4d4
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-08 15:29:20 -0300

    feat: este commit irá adicionar o export default do componente search form.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Button } from "./Button";
 export { default as Container } from "./Container";
 export { default as Header } from "./Header";
+export { default as SearchForm } from "./SearchForm";
 export { default as Summary } from "./Summary";
 export { default as SummaryCard } from "./SummaryCard";
 export { default as TransactionsTable } from "./TransactionsTable";

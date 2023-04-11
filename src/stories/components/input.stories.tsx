0188fb5f7e37fb8b5bc629bec2cd94f8ca038e67
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-11 19:47:13 -0300

    docs: este commit irá adicionar a doc (stories) do componente input.

---
+++ src/stories/components/input.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Input } from "../../components";
+
+export default {
+  title: "Components/Input",
+  component: Input,
+  tags: ["autodocs"],
+  argTypes: {},
+  parameters: {
+    layout: "centered"
+  }
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: "Search"
+};

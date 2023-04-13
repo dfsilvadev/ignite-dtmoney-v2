d1c1ca445cfa0b77ac3917221c9745a2fdbec156
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-13 20:26:46 -0300

    feat: este commit irá adicionar o export default do componente new transaction modal.

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Button } from "./Button";
 export { default as Container } from "./Container";
 export { default as Header } from "./Header";
 export { default as Input } from "./Input";
+export { default as NewTransactionModal } from "./NewTransactionModal";
 export { default as SearchForm } from "./SearchForm";
 export { default as Summary } from "./Summary";
 export { default as SummaryCard } from "./SummaryCard";

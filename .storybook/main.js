61400e12d88d759955dfb64a351065a6d70d8811
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-19 08:57:45 -0300

    chore: este commit irá configurar o absolute path do vite.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -14,5 +16,21 @@ module.exports = {
   },
   docs: {
     autodocs: "tag"
+  },
+  viteFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+      "@assets": path.resolve(__dirname, "../src/assets"),
+      "@components": path.resolve(__dirname, "../src/components"),
+      "@mocks": path.resolve(__dirname, "../src/mocks"),
+      "@screens": path.resolve(__dirname, "../src/screens"),
+      "@stories": path.resolve(__dirname, "../src/stories"),
+      "@styles": path.resolve(__dirname, "../src/styles"),
+      "@templates": path.resolve(__dirname, "../src/templates"),
+      "@tests": path.resolve(__dirname, "../src/tests"),
+      "@utils": path.resolve(__dirname, "../src/utils")
+    };
+    return config;
   }
 };

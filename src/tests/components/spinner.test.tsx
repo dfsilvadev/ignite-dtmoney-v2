c0e62cb0393e7b6e8fc4f58c271f971f2121d890
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-25 09:33:50 -0300

    test: este commit irá adicionar os testes do componente spinner.

---
+++ src/tests/components/spinner.test.tsx
@@ -0,0 +1,36 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "@/utils/tests";
+
+import Spinner from "@/components/Spinner";
+
+describe("Spinner", () => {
+  it("should render currectly", () => {
+    const { container } = renderWithTheme(<Spinner />);
+
+    expect(screen.getByRole("loader", { name: /loader/i })).toBeInTheDocument();
+
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        display: block;
+        margin: 15px auto;
+        position: relative;
+        background: #29292E;
+        box-shadow: -24px 0 #29292E, 24px 0 #29292E;
+        box-sizing: border-box;
+        -webkit-animation: shadowPulse 1.5s linear infinite;
+        animation: shadowPulse 1.5s linear infinite;
+      }
+
+      <div>
+        <span
+          aria-label="loader"
+          class="c0"
+          role="loader"
+        />
+      </div>
+    `);
+  });
+});

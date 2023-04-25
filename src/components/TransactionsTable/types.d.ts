2c5e7ddffb7af754b8dd58f1f464600f6e230119
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-25 09:39:00 -0300

    feat: este commit irá definir os types para o componente transactions.

--- src/components/TransactionsTable/types.d.ts
+++ src/components/TransactionsTable/types.d.ts
@@ -1,3 +1,10 @@
+import { Transaction } from "@/screens/Transactions/types";
+
 export type PriceHighlightProps = {
   variant: "income" | "outcome";
+};
+
+export type TransactionsTableProps = {
+  transactions: Transaction[];
+  isLoading: boolean;
 };

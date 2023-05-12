56b162cb3a8381dc0abf11da2e40bc74080e1383
Daniel Silva <dfsilva.dxp@gmail.com>
2023-05-12 14:24:14 -0300

    nit: add tests.

--- cypress/e2e/dtmoney-modal-spec.cy.js
+++ cypress/e2e/dtmoney-modal-spec.cy.js
@@ -14,9 +14,41 @@ describe("Home", () => {
     cy.wait(time);
   });
 
-  it("Checks if the modal is opened when the button is clicked.Check for the initial state", () => {
+  it("Checks if the modal is opened when the button is clicked.", () => {
     cy.get(".sc-duSInm > .sc-bgqQcB").click();
 
-    cy.contains("Nova Transação");
+    cy.get("[aria-label='overlay']").should("have.attr", "aria-hidden", "true");
+    cy.get("[role='dialog']").should("exist");
+  });
+
+  it("Checks if the modal is closed when clicking the X button", () => {
+    cy.get(".sc-duSInm > .sc-bgqQcB").click();
+
+    cy.get("[aria-label='overlay']").should("have.attr", "aria-hidden", "true");
+    cy.get("[role='dialog']").should("exist");
+
+    cy.get("[aria-label='close button'").should("exist");
+
+    cy.get('[aria-label="close button"').click();
+
+    cy.get("[aria-label='overlay']").should("not.exist");
+    cy.get("[role='dialog']").should("not.exist");
+  });
+
+  it("checks whether the form is submitted when filled out.", () => {
+    cy.get(".sc-duSInm > .sc-bgqQcB").click();
+
+    cy.get("[aria-label='overlay']").should("have.attr", "aria-hidden", "true");
+    cy.get("[role='dialog']").should("exist");
+
+    cy.get("input[name='description']").type("Pagamento da conta de internet");
+    cy.get("input[name='price']").type(109.99);
+    cy.get("input[name='category']").type("Internet");
+    cy.get(".dmVLBT").click();
+
+    cy.get(".sc-bgqQcB.gGySdV").click();
+
+    cy.get("[aria-label='overlay']").should("not.exist");
+    cy.get("[role='dialog']").should("not.exist");
   });
 });
